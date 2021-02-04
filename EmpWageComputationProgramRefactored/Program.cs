acbad0607b13b54fe8094053eee434154b30b42f
himanshunete <himanshuneteh@gmail.com>
2021-02-04 13:36:20 +0530

    [himanshunete]Ignore FilesAreIgnored

--- EmpWageComputationProgramRefactored/Program.cs
+++ EmpWageComputationProgramRefactored/Program.cs
@@ -26,7 +26,41 @@ namespace EmpWageComputationProgramRefactored
         public static void CheckingWage()
         {
             Console.WriteLine("Welcome To Employee Wage Computation Program");
-           
+            
+            Random rand = new Random();
+            int empCheck = rand.Next(0, 2);
+            if (empCheck == IS_PRESENT)
+            {
+                Console.WriteLine(" Employee is Present ");
+                while (totalEmpHrs <= MAX_NUM_OF_HOURS && totalWorkingDays <= NUM_OF_WORKING_DAYS)
+                {
+                    totalEmpHrs++;
+                    totalWorkingDays++;
+                    int workingTime = rand.Next(0, 2);
+                    switch (workingTime)
+                    {
+                        case IS_FULL_TIME:
+                            empHrs = 8;
+                            break;
+                        case IS_PART_TIME:
+                            empHrs = 4;
+                            break;
+                        default:
+                            empHrs = 0;
+                            break;
+                    }
+                    empWage = empHrs * EMP_RATE_PER_HOUR;
+                    totalEmpWage += empWage;
+                    Console.WriteLine(" Emp Daily Wage: " + empWage);
+                }
+                Console.WriteLine("Emp Total Wage: " + totalEmpWage);
+
+            }
+
+            else
+            {
+                Console.WriteLine(" Employee is Absent ");
+            }
 
         }
     }
